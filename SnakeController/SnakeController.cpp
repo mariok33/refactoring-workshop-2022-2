70c30568f8b6db74783ec2d1f503498288a7b866
Mariola Kisielewska <mario.kisielewska@gmail.com>
2022-03-30 17:06:20 +0200

    Pierwszy Static Cast

--- SnakeController/SnakeController.cpp
+++ SnakeController/SnakeController.cpp
@@ -216,21 +216,22 @@ Controller::Segment Controller::getNewHead() const
 void Controller::receive(std::unique_ptr<Event> e)
 {
     try {
-        handleTimePassed(*dynamic_cast<EventT<TimeoutInd> const&>(*e));
-    } catch (std::bad_cast&) {
-        try {
-            handleDirectionChange(*dynamic_cast<EventT<DirectionInd> const&>(*e));
-        } catch (std::bad_cast&) {
-            try {
-                handleFoodPositionChange(*dynamic_cast<EventT<FoodInd> const&>(*e));
-            } catch (std::bad_cast&) {
-                try {
-                    handleNewFood(*dynamic_cast<EventT<FoodResp> const&>(*e));
-                } catch (std::bad_cast&) {
-                    throw UnexpectedEventException();
-                }
-            }
-        }
+        handleTimePassed(*static_cast<EventT<TimeoutInd> const&>(*e));
+    } catch (std::bad_cast&) 
+    {
+    //     try {
+    //         handleDirectionChange(*dynamic_cast<EventT<DirectionInd> const&>(*e));
+    //     } catch (std::bad_cast&) {
+    //         try {
+    //             handleFoodPositionChange(*dynamic_cast<EventT<FoodInd> const&>(*e));
+    //         } catch (std::bad_cast&) {
+    //             try {
+    //                 handleNewFood(*dynamic_cast<EventT<FoodResp> const&>(*e));
+    //             } catch (std::bad_cast&) {
+    //                 throw UnexpectedEventException();
+        //         }
+        //     }
+        // }
     }
 }
 
--- SnakeController/SnakeController.hpp
+++ SnakeController/SnakeController.hpp
@@ -32,6 +32,7 @@ public:
 
     void receive(std::unique_ptr<Event> e) override;
 
+
 private:
     void handleTimePassed(const TimeoutInd&);
     void handleDirectionChange(const DirectionInd&);
